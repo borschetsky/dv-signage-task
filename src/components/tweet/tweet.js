f041a9fc35ede5e0a666e26c05cd09c7c1c0b20c
Viacheslav <wod.moshkin@gmail.com>
2020-02-16 18:20:13 +0200

    media queries for smaller devices

--- src/components/tweet/tweet.js
+++ src/components/tweet/tweet.js
@@ -1,53 +1,45 @@
-import React, { useRef } from 'react';
-import './tweet.css';
-import tweets from '../../mocks/tweets.json';
-import {Animated} from "react-animated-css";
+import React, { useRef } from "react";
+import "./tweet.css";
 
-const Tweet = (props) => {
-    
-    const imgRef = useRef(null);
-    // const tweet = tweets["tweets"][1]["tweets"][4];
-    // const images = tweets;
-    
-    // const res = images["tweets"][1]["tweets"];
-    // console.log(res);
-    const {tweet, topic} = props;
-    
+const Tweet = props => {
+  const imgRef = useRef(null);
 
-    const defaultImage = (e) => {
-        e.target.src = "https://recap-project.eu/wp-content/uploads/2017/02/default-user.jpg";
-    }
+  const { tweet, topic } = props;
 
-    const onErrorDisplayNone = (e) => {
-       imgRef.current.focus();
-       imgRef.current.style.display = 'none';
-    }
+  const defaultImage = e => {
+    e.target.src =
+      "https://recap-project.eu/wp-content/uploads/2017/02/default-user.jpg";
+  };
 
-    return (
-       
-            <div className="post-container">
-            <img src={tweet.img}  ref={imgRef} onError={onErrorDisplayNone}></img>
-            <div className="info">
-                <div className="bar">
-                    <span>
-                    {topic}
-                    </span>
-                </div>
-                <p>{tweet.text}</p>
-                <div className="user-info">
-                    <div className="user-image">
-                        <img onError={defaultImage} src={tweet.userImg} alt={tweet.userImg}></img>
-                    </div>
-                    <div className="user-name">
-                        <h5>{tweet.user}</h5>
-                        {tweet.screenName}
-                    </div>
-                </div>
-            </div>
+  const onErrorDisplayNone = e => {
+    imgRef.current.focus();
+    imgRef.current.style.display = "none";
+  };
+
+  return (
+    <div className="post-container">
+      <img src={tweet.img} ref={imgRef} onError={onErrorDisplayNone} alt="post"></img>
+      <div className="info">
+        <div className="bar">
+          <span>{topic}</span>
         </div>
-        
-        
-    );
-}
+        <p>{tweet.text}</p>
+        <div className="user-info">
+          <div className="user-image">
+            <img
+              onError={defaultImage}
+              src={tweet.userImg}
+              alt={tweet.userImg}
+            ></img>
+          </div>
+          <div className="user-name">
+            <h5>{tweet.user}</h5>
+            {tweet.screenName}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Tweet;
--- src/pages/feed/feed.js
+++ src/pages/feed/feed.js
@@ -1,30 +1,41 @@
-import React from 'react';
-import ScrollAnimation from 'react-animate-on-scroll';
-import TweetDark from '../../components/tweet-dark';
-import Tweet from '../../components/tweet';
-import { parseTweets} from '../../helpers/jsonHelper';
+import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
+import TweetDark from "../../components/tweet-dark";
+import Tweet from "../../components/tweet";
+import { parseTweets } from "../../helpers/jsonHelper";
 
-const Feed = (props) => {
-    const {type} = props;
+const Feed = props => {
+  const { type } = props;
 
-    const preparedTweetsTriple = [...parseTweets(), ...parseTweets(),...parseTweets(), ...parseTweets()].sort(() => Math.random() - 0.5);
+  const preparedTweetsTriple = [
+    ...parseTweets(),
+    ...parseTweets(),
+    ...parseTweets(),
+    ...parseTweets()
+  ].sort(() => Math.random() - 0.5);
 
-    const preparedTweets = preparedTweetsTriple.map((el, i) => {
-      const currentTweet = 
-        (type === 'dark' ? 
-          (<TweetDark tweet={el} topic={el.topic} />) : (<Tweet tweet={el} topic={el.topic} />));
-      return (
-        <ScrollAnimation animateIn="fadeIn" animateOut='fadeOut' key={i} delay={11 * i}>
-          {currentTweet}
-        </ScrollAnimation>
+  const preparedTweets = preparedTweetsTriple.map((el, i) => {
+    const currentTweet =
+      type === "dark" ? (
+        <TweetDark tweet={el} topic={el.topic} />
+      ) : (
+        <Tweet tweet={el} topic={el.topic} />
       );
-    });
-
     return (
-        <div>
-            {preparedTweets}
-        </div>
+      <ScrollAnimation
+        animateIn="flipInY"
+        animateOut="fadeOut"
+        key={i}
+        delay={11 * (Math.random() * preparedTweetsTriple.length)}
+        animationInDuration="1500"
+        offset={100}
+      >
+        {currentTweet}
+      </ScrollAnimation>
     );
-}
+  });
+
+  return <div>{preparedTweets}</div>;
+};
 
 export default Feed;
