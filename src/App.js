cc124a6af123fce17a32fd224866004f0488550c
Viacheslav <wod.moshkin@gmail.com>
2020-02-16 18:26:56 +0200

    format code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Feed from "./pages/feed";
 
 function App() {
   return (
     <Router>
       <Switch>
-      
         <Route exact path="/">
           <div className="flex-container">
             <Feed type={"dark"} />
